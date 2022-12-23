96a8abc4b594056887585250e35580f49bc0f28a
Admin <kqadb@mail.ru>
2022-12-24 00:02:58 +0500

    hi

--- src/homeWork.java
+++ src/homeWork.java
@@ -4,7 +4,40 @@ public class homeWork {
 
 
 
-        
+int num = 0;
+while (num < 10){
+    num++;
+    System.out.print(num + " ");
+
+}
+        System.out.println();
+
+for (int i = 10; i > 0; i-- ){
+    System.out.print(i + " ");
+}
+        System.out.println();
+
+int day = 5;
+for (int d = day; d <= 31; d+=7 ){
+
+        System.out.println( "today is friday " + d + " need your report" );
+    }
+
+int currentYear = 2021;
+
+int start = currentYear - 200;
+int end = currentYear + 100;
+for (int year = start; year < end; year ++){
+    if (year % 79 == 0){
+        System.out.println(year);
+
+    }
+}
+
+
+
+
+
     }
 
 }
